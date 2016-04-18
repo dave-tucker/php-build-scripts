d74b5c709483922baef4ce96a462be3d0602328a
Dave Tucker <dave@dtucker.co.uk>
2016-04-18 11:32:23 +0100

    Fix IOS detection

--- installer.sh
+++ installer.sh
@@ -249,11 +249,9 @@ else
 	echo -n "[3/3] Obtaining PHP:"
 	echo " detecting if build is available..."
 	if [ "$forcecompile" = "off" ] && [ "$(uname -s)" = "Darwin" ]; then
-		set +e
 		UNAME_M=$(uname -m)
-		IS_IOS=$(expr match $UNAME_M 'iP[a-zA-Z0-9,]*' 2> /dev/null)
-		set -e
-		if [ "$IS_IOS" -gt 0 ]; then
+		# Test for IOS
+		if [ $(echo $UNAME_M | grep -qe 'iP[a-zA-Z0-9,]*') ]; then
 			rm -r -f bin/ >> /dev/null 2>&1
 			echo -n "[3/3] iOS PHP build available, downloading $IOS_BUILD.tar.gz..."
 			download_file "https://dl.bintray.com/pocketmine/PocketMine/$IOS_BUILD.tar.gz" | tar -zx > /dev/null 2>&1
